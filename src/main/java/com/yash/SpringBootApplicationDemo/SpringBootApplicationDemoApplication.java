19888d5dcbe0ad0e050fe5abd83a6d456d242883
Puspendra Kumar Sharma <puspendra.sharma@yash.com>
2019-05-31 12:33:11 +0530

    First commit

---
+++ src/main/java/com/yash/SpringBootApplicationDemo/SpringBootApplicationDemoApplication.java
@@ -0,0 +1,14 @@
+package com.yash.SpringBootApplicationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationDemoApplication.class, args);
+	}
+
+}
+
